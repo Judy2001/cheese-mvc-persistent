5e302d3f3fd652c5515fd401b27627876763f63c
Judy@LAPTOP <judylight2001@gmail.com>
2018-11-11 18:59:35 -0800

    Final changes.

--- src/main/java/org/launchcode/models/Menu.java
+++ src/main/java/org/launchcode/models/Menu.java
@@ -1,10 +1,9 @@
 package org.launchcode.models;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
 import java.util.List;
+
 
 @Entity
 public class Menu {
